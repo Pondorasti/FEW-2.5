a12251ef1a1fa9518a70816ea3ae04e9a819de35
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-11-11 21:41:11 -0800

    feat: ring visualizer & mic permissions

--- Class Work/audio-visualizer/script.js
+++ Class Work/audio-visualizer/script.js
@@ -1,19 +1,46 @@
 const canvas = document.getElementById("renderer")
 const ctx = canvas.getContext("2d")
 
+let micAllowed = false
+let isListening = false
+
+let stream
 let analyser
 let frequencyData
+
+async function requestStartAudio() {
+  if (!micAllowed) {
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+      isListening = true
+      startAudio()
+    } catch (err) {
+      alert("Microphone permissions denied!")
+    }
+  } else if (isListening) {
+    isListening = false
+    // TODO: stop
+  } else if (!isListening) {
+    isListening = true
+    startAudio()
+  }
+}
+
 function startAudio() {
   const audio = new Audio()
 
   // get context
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 
-  audio.src = "bird-whistling-a.wav"
-  // audio.src = "sacred-song.mp3"
-
+  // create analyser
   analyser = audioCtx.createAnalyser()
-  const source = audioCtx.createMediaElementSource(audio)
+  analyser.fftSize = 256
+
+  // create audio source
+  // audio.src = "bird-whistling-a.wav"
+  // audio.src = "sacred-song.mp3"
+  // const source = audioCtx.createMediaElementSource(audio)
+  const source = audioCtx.createMediaStreamSource(stream)
 
   source.connect(analyser)
   analyser.connect(audioCtx.destination)
@@ -29,7 +56,7 @@ function startAudio() {
 function render() {
   const centerX = canvas.width / 2
   const centerY = canvas.height / 2
-  const radius = canvas.width * 0.125
+  const radius = canvas.width * 0.25
 
   analyser.getByteFrequencyData(frequencyData)
 
@@ -41,28 +68,28 @@ function render() {
 }
 
 function ringRenderer(frequencyData, ctx, centerX, centerY, radius) {
+  const barWidth = 2.5
+  const barHeight = 2.5
+
+  ctx.lineWidth = barWidth
+
   const numberOfBars = frequencyData.length
-  const maxBarLength = (canvas.width - radius) / 2
   const barStep = (2 * Math.PI) / numberOfBars
+  const freqStep = Math.floor(frequencyData.length / numberOfBars)
 
   ctx.beginPath()
 
-  // draw circle
-  ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
-  ctx.strokeStyle = "#f00"
-  ctx.stroke()
+  for (let index = 0; index < frequencyData.length; index += freqStep) {
+    const amplitude = (frequencyData[index] / 6 + barHeight) / 2
 
-  // draw bars
-  frequencyData.forEach((value, index) => {
-    const barLength = (value / 256) * maxBarLength
-
-    const x1 = centerX + radius * Math.cos(barStep * index)
-    const y1 = centerY + radius * Math.sin(barStep * index)
-    const x2 = centerX + (radius + barLength) * Math.cos(barStep * index)
-    const y2 = centerY + (radius + barLength) * Math.sin(barStep * index)
+    const x1 = centerX + (radius - amplitude) * Math.cos(barStep * index)
+    const y1 = centerY + (radius - amplitude) * Math.sin(barStep * index)
+    const x2 = centerX + (radius + amplitude) * Math.cos(barStep * index)
+    const y2 = centerY + (radius + amplitude) * Math.sin(barStep * index)
 
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
-  })
+  }
+
   ctx.stroke()
 }
