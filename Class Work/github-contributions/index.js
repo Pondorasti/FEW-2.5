0e5cce1934556ce1008d15d2122ffcb2c2fc5bd7
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-12-03 20:26:39 -0800

    finish: github-contributions

--- Class Work/github-contributions/index.js
+++ Class Work/github-contributions/index.js
@@ -2,20 +2,34 @@ async function handleData() {
   const payload = await d3.json("payload.json")
   const contributions = payload.data.user.contributionsCollection.contributionCalendar.weeks
 
-  const svg = d3.select("#svg").attr("width", 740).attr("height", 400)
-  const chartContainer = svg.append("g")
+  const chartWidth = 740
+  const chartHeight = 88
+  const topMargin = 12
+  const leftMargin = 24
 
-  const chartPath = chartContainer
-    .selectAll("g.chartPath")
+  const fontFamily =
+    "ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'"
+
+  const svg = d3
+    .select("#svg")
+    .attr("width", chartWidth + leftMargin)
+    .attr("height", chartHeight + topMargin)
+
+  // Chart
+  const chartContainer = svg
+    .append("g")
+    .attr("id", "chartContainer")
+    .attr("transform", (d, i) => `translate(${leftMargin}, ${topMargin})`)
+  const weekPaths = chartContainer
+    .selectAll("g")
     .data(contributions)
     .enter()
     .append("g")
-    .classed("chartPath", true)
     .attr("transform", (d, i) => `translate(${i * 14}, 0)`)
 
-  const weeklyGroups = chartPath
+  weekPaths
     .selectAll("rect")
-    .data((d, i, j) => {
+    .data((d) => {
       return d.contributionDays
     })
     .enter()
@@ -26,11 +40,55 @@ async function handleData() {
     .attr("height", 10)
     .attr("rx", 2)
     .attr("ry", 2)
-    .attr("style", "shape-rendering: geometricPrecision")
-    .attr("style", "outline: 1px solid rgba(27, 31, 35, 0.06)")
-    .attr("style", "outline-offset: -1px; border-radius: 2px")
-
+    .attr(
+      "style",
+      "shape-rendering: geometricPrecision; outline: 1px solid rgba(27, 31, 35, 0.06); outline-offset: -1px; border-radius: 2px"
+    )
     .attr("fill", (d) => d.color)
+
+  // Top Axis
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ]
+  // const xScale = d3.scaleLinear().domain([1, 12]).range([0, chartWidth])
+  const topAxis = svg.append("g").attr("id", "topAxis")
+  topAxis
+    .selectAll("text")
+    .data(months)
+    .enter()
+    .append("text")
+    .text((d) => d)
+    .attr("x", (d, i) => leftMargin + (chartWidth / months.length) * i) // leftMargin + xScale(i)
+    .attr("y", (d) => 7)
+    .style("font-size", "9px")
+    .style("font-family", fontFamily)
+
+  // Left Axis
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  // const yScale = d3.scalePoint().domain(days).range([0, chartHeight])
+  const leftAxis = svg.append("g").attr("id", "leftAxis")
+  leftAxis
+    .selectAll("text")
+    .data(days)
+    .enter()
+    .append("text")
+    .text((d) => d)
+    .attr("x", (d) => 0)
+    .attr("y", (d, i) => topMargin + 9 + (chartHeight / days.length) * i) // topMargin + 9 + yScale(i)
+    .attr("style", (d, i) => `display: ${i % 2 === 1 ? "block" : "none"}`)
+    .style("font-size", "9px")
+    .style("font-family", fontFamily)
 }
 
 handleData()
